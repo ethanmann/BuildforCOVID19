77c280c9b663a1d3bd442c297647fb64a027739d
Ethan Mann <e.milomann@gmail.com>
2020-03-27 01:11:39 -0500

    WIP - framework for search, querying, results, business sub-pages

--- WIP/application.py
+++ WIP/application.py
@@ -5,6 +5,7 @@ import sheets_access as drive
 from google_form import GOOGLE_FORM_URL
 
 from wsgiref.simple_server import make_server
+from cgi import parse_qs, escape
 from wsgi_static_middleware import StaticMiddleware
 import os
 
@@ -44,6 +45,67 @@ def render_template(filename, substitutions):
 def application(environ, start_response):
     path    = environ['PATH_INFO']
     method  = environ['REQUEST_METHOD']
+
+
+
+    # https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_and_retrieving_form_data
+    # https://docs.plone.org/develop/plone/serving/http_request_and_response.html
+    # http://wsgi.tutorial.codepoint.net/parsing-the-request-post
+    # the environment variable CONTENT_LENGTH may be empty or missing
+    try:
+        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+    except (ValueError):
+        request_body_size = 0
+
+    # When the method is POST the variable will be sent
+    # in the HTTP request body which is passed by the WSGI server
+    # in the file like wsgi.input environment variable.
+    request_body = environ['wsgi.input'].read(request_body_size)
+    d = parse_qs(request_body)
+
+    form_type = { "all":"Any Business Type",
+                  "food":"Food",
+                  "store":"Store",
+                  "entertainment":"Entertainment",
+                  "fitness":"Fitness",
+                  "others":"Others" }
+    ALL_FORM_TYPES = ["Art/Jewelry",
+                      "Bakery",
+                      "Bar",
+                      "Bookstore",
+                      "Cafe",
+                      "Car Repairs",
+                      "Clothing/Shoe Store",
+                      "Electronics",
+                      "Entertainment Venue",
+                      "Florist",
+                      "Gas Station",
+                      "Groceries/Produce",
+                      "Gym",
+                      "Hardware Store",
+                      "Homegoods",
+                      "House Repairs",
+                      "Restaurant",
+                      "Salon/Haircuts",
+                      "Outdoor Supplies"]
+    site_type_map_to_form_types = {
+                  "all":ALL_FORM_TYPES,
+                  "food":["Bakery","Cafe","Groceries/Produce","Restaurant"],
+                  "store":["Art/Jewelry",'Bookstore',"Clothing/Shoe Store",
+                           "Electronics","Florist",'Homegoods',"Outdoor Supplies"],
+                  "entertainment":["Entertainment Venue"],
+                  "fitness":["Gym"],
+                  "others":["Car Repairs","Gas Station","Bar","House Repairs",
+                            "Salon/Haircuts"]
+    }
+    def get_selected_options(selected_type):
+        selected_options = []
+        for type in form_type:
+            tuple = (type, "selected" if type == selected_type else "", form_type[type])
+            selected_options.append(tuple)
+        print(selected_options)
+        return selected_options
+
     if method == 'POST':
         response = ''
         try:
@@ -58,10 +120,22 @@ def application(environ, start_response):
                 # for now, we just link to the results page (without any param)
                 # passing in the sheets data (eventually will have more params)
                 print('should be returning results now!')
+                print(d)
+                zip = d.get('zip',['91126'])[0]
+                # this makes sure they dont type "City, State"
+                city = (d.get('city',['Pasadena'])[0]).split(',')[0]
+                type = d['type'][0]
+                # type = d.get('type',['all'])
+                drive_data = drive.run_sheets_scrape(zip, city, type, site_type_map_to_form_types)
                 response = render_template('results.html',
-                                           {'data': drive.run_sheets_scrape(),
+                                           {'data': drive_data,
+                                            'data_len': len(drive_data),
                                             'parent':'/static/',
-                                            'form':GOOGLE_FORM_URL})
+                                            'form':GOOGLE_FORM_URL,
+                                            'zip':zip,
+                                            'city':city,
+                                            'type':type,
+                                            'options':get_selected_options(type)})
 
         except (TypeError, ValueError):
             logger.warning('Error retrieving request body for async work.')
@@ -73,10 +147,18 @@ def application(environ, start_response):
         elif path == '/about':
             response = render_template('about.html', {'parent':'/static/',
                                                         'form':GOOGLE_FORM_URL})
+        elif '/business/' in path:
+            row = path.split('business/')[-1]
+            record = drive.get_row_record(int(row))
+            print(record)
+            response = render_template('listing.html', {'parent':'/static/',
+                                                        'form':GOOGLE_FORM_URL})
+                                                        # 'record':record})
         else:
             # passing in the static folder which contains the static resources
             response = render_template('index.html', {'parent':'/static/',
-                                                      'form':GOOGLE_FORM_URL})
+                                                      'form':GOOGLE_FORM_URL,
+                                                      'options':get_selected_options('all')})
     status = '200 OK'
     headers = [('Content-type', 'text/html')]
 
--- WIP/sheets_access.py
+++ WIP/sheets_access.py
@@ -2,16 +2,80 @@
 import pygsheets
 from sheets_access_secret import *
 
-def run_sheets_scrape():
-    # print(gc.spreadsheet_titles())
-    #
-    # return
-
+def get_sheet():
     # authorize right before it's needed!
     gc = pygsheets.authorize(service_file=CREDENTIALS)
 
     spreadsheet = gc.open_by_key(SHEET_KEY)
     worksheet1 = spreadsheet.sheet1
 
+    return worksheet1
+
+def get_row_record(row, worksheet=None):
+    if worksheet == None:
+        worksheet = get_sheet()
+
+    header_arr = worksheet.get_row(1, include_tailing_empty=False)
+    row_arr = worksheet.get_row(row, include_tailing_empty=False)
+
+    record = {}
+    for i in range(len(header_arr)):
+        key = header_arr[i]
+        val = row_arr[i]
+        record[key] = val
+    record['Row'] = row
+    return record
+
+def run_sheets_scrape(zip, city, type, site_type_map_to_form_types):
+    # old debugging
+    # print(gc.spreadsheet_titles())
+    # return
+
+    worksheet = get_sheet()
+
     # https://pygsheets.readthedocs.io/en/stable/worksheet.html
-    return [s for s in worksheet1.get_col(RESPONSE_COLUMN) if s != '']
+    # return [s for s in worksheet1.get_col(RESPONSE_COLUMN) if s != '']
+
+    # https://pygsheets.readthedocs.io/en/stable/worksheet.html#pygsheets.Worksheet.get_all_records
+    # makes sure everything is set to be a string with numericise_data as False
+    records = worksheet.get_all_records(numericise_data=False)
+
+    print(records)
+    if DOUBLE_HEADER:
+        records = records[1:]
+    print(records)
+    new_records = []
+
+    if DOUBLE_HEADER:
+        curr_row = 3
+    else:
+        curr_row = 2
+
+    for record in records:
+        # start by adding row to the record
+        record['Row'] = curr_row
+        curr_row += 1
+
+        # first, check if there is a City match
+        if str(record['City']).lower() != str(city).lower():
+            continue
+
+        # check if the site type matches any of the form types
+        match = False
+        record_types = str(record['Type']).lower()
+        for match_type in site_type_map_to_form_types[type]:
+            if match_type.lower() in record_types:
+                match = True
+                break
+
+        if match == False:
+            continue
+
+        # then, check if there is a ZIP match
+        # eventually, do a distance check instead of a equality match
+        if str(record['ZIP']).lower() != str(zip).lower():
+            new_records.append(record)
+        else:
+            new_records.insert(0, record)
+
+    return new_records
